e8885f3023a4e5be81a152fcaa5bda0becb224f8
Darya <knyazevadarya2016@gmail.com>
2022-07-25 15:25:16 +0400

    тест

--- bboard/views.py
+++ bboard/views.py
@@ -1,4 +1,4 @@
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Здесь будет выведен список объявлений.")
+    return HttpResponse("Здесь будет выведен список объявлений. А может и не будет...")
